52a86151d6f0ac8aa4087e47f99156f9f34793e9
Arnold Trakhtenberg <arnold@launchdarkly.com>
2020-01-06 10:51:34 -0500

    Add configuration menu workflow (#29)
    * Break out models an api, refactor classes into singletons
    
    * lint
    
    * Break out providers into a seperate file
    
    * clean up flagStore
    
    * cleanup api
    
    * use dependency injection for configuration
    
    * lint
    
    * add api methods
    
    * update models
    
    * add support for state-based access token
    
    * create ConfigurationMenu
    
    * cleanup
    
    * fix
    
    * lint
    
    * imports
    
    * add sample license
    
    * update api during configuration flow
    
    * state transitions
    
    * lint

--- src/api.ts
+++ src/api.ts
@@ -2,14 +2,37 @@ import * as rp from 'request-promise-native';
 import * as url from 'url';
 
 import { Configuration } from './configuration';
-import { Flag } from './models';
+import { Resource, Project, Environment, Flag } from './models';
 
 // LaunchDarklyAPI is a wrapper around request-promise-native for requesting data from LaunchDarkly's REST API. The caller is expected to catch all exceptions.
 export class LaunchDarklyAPI {
-	config: Configuration;
+	private readonly config: Configuration;
 
 	constructor(config: Configuration) {
 		this.config = config;
+	}
+
+	async getAccount() {
+		const options = this.createOptions('account');
+		const account = await rp(options);
+		return JSON.parse(account);
+	}
+
+	async getProjects(): Promise<Array<Project>> {
+		const options = this.createOptions('projects');
+		const data = await rp(options);
+		const projects = JSON.parse(data).items;
+		projects.forEach((proj: Project) => {
+			proj.environments = proj.environments.sort(sortNameCaseInsensitive);
+			return proj;
+		});
+		return projects.sort(sortNameCaseInsensitive);
+	}
+
+	async getEnvironment(projectKey: string, envKey: string): Promise<Environment> {
+		const options = this.createOptions(`projects/${projectKey}/environments/${envKey}`);
+		const data = await rp(options);
+		return JSON.parse(data);
 	}
 
 	async getFeatureFlag(projectKey: string, flagKey: string, envKey?: string): Promise<Flag> {
@@ -28,3 +51,7 @@ export class LaunchDarklyAPI {
 		};
 	}
 }
+
+const sortNameCaseInsensitive = (a: Resource, b: Resource) => {
+	return a.name.toLowerCase().localeCompare(b.name.toLowerCase());
+};
--- src/extension.ts
+++ src/extension.ts
@@ -1,25 +1,52 @@
 'use strict';
 
-import { workspace, ExtensionContext, ConfigurationChangeEvent } from 'vscode';
+import { commands, window, workspace, ExtensionContext, ConfigurationChangeEvent } from 'vscode';
 
 import { FlagStore } from './flagStore';
 import { Configuration } from './configuration';
 import { register as registerProviders } from './providers';
+import { LaunchDarklyAPI } from './api';
 
-const config = new Configuration();
-const flagStore = new FlagStore(config);
+let config: Configuration;
+let flagStore: FlagStore;
 
 export function activate(ctx: ExtensionContext) {
-	workspace.onDidChangeConfiguration((e: ConfigurationChangeEvent) => {
+	config = new Configuration(ctx);
+
+	const validationError = config.validate();
+	switch (validationError) {
+		case 'unconfigured':
+			window
+				.showInformationMessage('To enable the LaunchDarkly extension, select your desired environment.', 'Configure')
+				.then(item => item && commands.executeCommand('extension.configureLaunchDarkly'));
+
+		case 'legacy':
+			window
+				.showWarningMessage(
+					'Your LaunchDarkly extension configuration has been deprecated and may not work correctly. Please reconfigure the extension.',
+					'Configure',
+				)
+				.then(item => {
+					item === 'Configure'
+						? commands.executeCommand('extension.configureLaunchDarkly')
+						: ctx.globalState.update('legacyNotificationDismissed', true);
+				});
+	}
+
+	const api = new LaunchDarklyAPI(config);
+	flagStore = new FlagStore(config, api);
+
+	// Handle manual changes to extension configuration
+	workspace.onDidChangeConfiguration(async (e: ConfigurationChangeEvent) => {
 		if (e.affectsConfiguration('launchdarkly')) {
-			config.reload();
-			flagStore.reload(e);
+			await config.reload();
+			await flagStore.reload(e);
 		}
 	});
 
-	registerProviders(ctx, config, flagStore);
+	registerProviders(ctx, config, flagStore, api);
 }
 
 export function deactivate() {
-	flagStore.stop();
+	flagStore && flagStore.stop();
 }
