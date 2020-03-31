a0cf7b93659dc216bc1d1e3bc7d63fdfe5496798
Dan O'Brien <dobrien@launchdarkly.com>
2020-03-30 20:43:41 -0400

    basic flag toggle working

--- src/flagStore.ts
+++ src/flagStore.ts
@@ -1,8 +1,7 @@
 import { ConfigurationChangeEvent, commands, window } from 'vscode';
-import { LDFeatureStore, LDStreamProcessor } from 'launchdarkly-node-server-sdk';
 import InMemoryFeatureStore = require('launchdarkly-node-server-sdk/feature_store');
-import StreamProcessor = require('launchdarkly-node-server-sdk/streaming');
-import Requestor = require('launchdarkly-node-server-sdk/requestor');
+import LaunchDarkly = require('launchdarkly-node-server-sdk');
+
 import { debounce } from 'lodash';
 
 import { Flag, FlagConfiguration, FlagWithConfiguration } from './models';
@@ -14,12 +13,12 @@ const DATA_KIND = { namespace: 'features' };
 
 export class FlagStore {
 	private readonly config: Configuration;
-	private readonly store: LDFeatureStore;
+	private readonly store: LaunchDarkly.LDFeatureStore;
 	private readonly flagMetadata: { [key: string]: Flag } = {};
 
 	private readonly api: LaunchDarklyAPI;
 	private readonly streamingConfigOptions = ['accessToken', 'baseUri', 'streamUri', 'project', 'env'];
-	private updateProcessor: LDStreamProcessor;
+	public ldClient: LaunchDarkly.LDClient
 
 	constructor(config: Configuration, api: LaunchDarklyAPI) {
 		this.config = config;
@@ -43,41 +42,31 @@ export class FlagStore {
 		}
 	}, 200);
 
-	async start() {
+	start(): LaunchDarkly.LDClient {
+
 		if (!['accessToken', 'baseUri', 'streamUri', 'project', 'env'].every(o => !!this.config[o])) {
 			console.warn('LaunchDarkly extension is not configured. Language support is unavailable.');
 			return;
 		}
 
-		const sdkKey = await this.getLatestSDKKey();
-		if (!sdkKey) {
-			return;
-		}
+		var sdkKey: string
 
-		const ldConfig = this.ldConfig();
-		this.updateProcessor = StreamProcessor(sdkKey, ldConfig, Requestor(sdkKey, ldConfig));
-		return new Promise((resolve, reject) => {
-			this.updateProcessor.start(err => {
-				if (err) {
-					let errMsg: string;
-					if (err.message) {
-						errMsg = `Error retrieving feature flags: ${err.message}.`;
-					} else {
-						console.error(err);
-						errMsg = `Unexpected error retrieving flags.`;
-					}
-					reject(errMsg);
-					return;
-				}
-				resolve();
-				process.nextTick(function() {});
-			});
-		});
+		this.getLatestSDKKey().then((sdkKey) => {
+			sdkKey = sdkKey
+			const ldConfig = this.ldConfig();
+
+			console.log(sdkKey)
+			let ldClient: LaunchDarkly.LDClient = LaunchDarkly.init(sdkKey, ldConfig)
+			ldClient.waitForInitialization().then(client => {
+				this.ldClient = client
+				return client
+			})
+		})
 	}
 
 	stop(): Promise<void> {
 		return new Promise(resolve => {
-			this.updateProcessor && this.updateProcessor.stop();
+			this.ldClient && this.ldClient.close();
 			this.store.init({ features: [] }, resolve);
 		});
 	}
@@ -85,6 +74,7 @@ export class FlagStore {
 	private async getLatestSDKKey() {
 		try {
 			const env = await this.api.getEnvironment(this.config.project, this.config.env);
+			console.log(env)
 			return env.apiKey;
 		} catch (err) {
 			if (err.statusCode === 404) {
@@ -107,12 +97,6 @@ export class FlagStore {
 			baseUri: this.config.baseUri,
 			streamUri: this.config.streamUri,
 			featureStore: this.store,
-			logger: {
-				debug: console.log,
-				warn: console.warn,
-				error: console.error,
-			},
-			userAgent: 'VSCodeExtension/' + PACKAGE_JSON.version,
 		};
 	}
 
--- src/flagsView.ts
+++ src/flagsView.ts
@@ -1,24 +1,24 @@
 import * as vscode from 'vscode';
-import { Flag, Rule, Clause } from './models';
-import { FlagStore } from './flagStore';
+import { FeatureFlag } from './models';
 import { LaunchDarklyAPI } from './api';
-import { Configuration } from './configuration';
-import { join } from 'path';
+import { Configuration, getIsTreeviewEnabled } from './configuration';
+import { FlagStore } from './flagStore';
 
 export class ldFeatureFlagsProvider implements vscode.TreeDataProvider<FlagValue> {
   private readonly api: LaunchDarklyAPI;
   private config: Configuration;
-  private flags: Array<Flag>;
-  private flagValues: Array<FlagValue>;
   private flagStore: FlagStore;
+  private flagValues: Array<FlagValue>;
+  private ctx: vscode.ExtensionContext;
   private _onDidChangeTreeData: vscode.EventEmitter<FlagValue | undefined> = new vscode.EventEmitter<FlagValue | undefined>();
 	readonly onDidChangeTreeData: vscode.Event<FlagValue | undefined> = this._onDidChangeTreeData.event;
 
-  constructor(api: LaunchDarklyAPI, config: Configuration, flagStore: FlagStore) {
+  constructor(api: LaunchDarklyAPI, config: Configuration, flagStore: FlagStore, ctx: vscode.ExtensionContext) {
     this.api = api;
     this.config = config;
+    this.ctx = ctx;
     this.flagStore = flagStore;
-    this.getFlags()
+    this.start()
   }
 
 	refresh(): void {
@@ -47,41 +47,95 @@ export class ldFeatureFlagsProvider implements vscode.TreeDataProvider<FlagValue
     const flags = await this.api.getFeatureFlags(this.config.project)
     let flagValues = []
     for (const flag of flags) {
-      let flagUri = this.config.baseUri + flag.environments[this.config.env]._site.href
-      let item = new FlagValue(flag.name, vscode.TreeItemCollapsibleState.Collapsed,
-        [
-          new FlagValue(`Open in Browser`, vscode.TreeItemCollapsibleState.None, [], "flagViewBrowser", flagUri),
-          new FlagValue(`key: ${flag.key}`, vscode.TreeItemCollapsibleState.None, [], "flagViewKey"),
-          new FlagValue(`on: ${flag.environments[this.config.env].on}`, vscode.TreeItemCollapsibleState.None),
-          new FlagValue(`description: ${flag.description ? flag.description : ""}`, vscode.TreeItemCollapsibleState.None),
-          new FlagValue(`prerequisites: ${flag.environments[this.config.env].prerequisites}`, vscode.TreeItemCollapsibleState.None),
-        ]
-      )
+      let item = this.flagToValues(flag)
+      flagValues.push(item)
+    }
+    this.flagValues = flagValues
+    this.refresh()
+  }
 
-      var targets: Array<FlagValue> = []
-      if (flag.environments[this.config.env].targets) {
-      for(let i = 0; i<flag.environments[this.config.env].targets.length; i++) {
-          let curTarget = flag.environments[this.config.env].targets[i]
-          targets.push(
-            new FlagValue(`variation: ${flag.variations[curTarget.variation].name ? flag.variations[curTarget.variation].name : flag.variations[curTarget.variation].value}`, vscode.TreeItemCollapsibleState.None),
-            new FlagValue(`values: ${curTarget.values}`, vscode.TreeItemCollapsibleState.None),
-          )
+  async start() {
+
+    this.ctx.subscriptions.push(
+      vscode.commands.registerCommand('ldFeatureFlags.copyKey', (node: FlagValue) => vscode.env.clipboard.writeText(node.label.split(":")[1].trim())),
+      vscode.commands.registerCommand('ldFeatureFlags.openBrowser', (node: FlagValue) => vscode.env.openExternal(vscode.Uri.parse(node.uri))),
+      vscode.commands.registerCommand('ldFeatureFlags.toggleFlag', (node: FlagValue) => this.toggleFlag(node)),
+      vscode.commands.registerCommand('ldFeatureFlags.refreshEntry', () => this.refresh()),
+      registerTreeviewRefreshCommand(this)
+
+    )
+
+    var that = this;
+    if (this.flagStore.ldClient === undefined) {
+      setTimeout(function() {
+      that.flagStore.ldClient.on('update', function (flags) {
+        that.api.getFeatureFlagNew(that.config.project, flags.key, that.config.env).then((flag) => {
+          for (let i = 0; i < that.flagValues.length; i++) {
+            if (that.flagValues[i].label === flag.name) {
+              that.flagValues[i] = that.flagToValues(flag)
+              that.refresh()
+              break
+          }
         }
-      }
-      item.children.push(new FlagValue(`targets:`, targets.length > 0 ? vscode.TreeItemCollapsibleState.Collapsed : vscode.TreeItemCollapsibleState.None, targets))
+      })})
+    }, 5000)}
 
-      var variations: Array<FlagValue> = []
-      for(let i = 0; i<flag.variations.length; i++) {
-        variations.push(
-          new FlagValue(`name: ${flag.variations[i].name ? flag.variations[i].name : flag.variations[i].value}`, vscode.TreeItemCollapsibleState.None),
-          new FlagValue(`value: ${flag.variations[i].value}`, vscode.TreeItemCollapsibleState.None),
-          new FlagValue(`description: ${flag.variations[i].description ? flag.variations[i].description : ""}`, vscode.TreeItemCollapsibleState.None)
+    this.getFlags()
+  }
+
+  async toggleFlag(flag: FlagValue) {
+    let curValue = JSON.parse(flag.label.split(":")[1].trim())
+    try {
+      var updatedFlag = await this.api.patchFeatureFlagOn(this.config.project, flag.flagKey, !curValue, this.config.env)
+    } catch(e) {
+      vscode.window.showInformationMessage("LaunchDarkly Toggle Flag Error: " + e);
+    }
+    for(let i = 0; i<this.flagValues.length; i++) {
+      if (this.flagValues[i].label == flag.flagParentName) {
+        this.flagValues[i] = this.flagToValues(updatedFlag)
+        break
+      }
+    }
+    this.refresh()
+  }
+
+  private flagToValues(flag: FeatureFlag): FlagValue {
+    let flagUri = this.config.baseUri + flag.environments[this.config.env]._site.href
+    let item = new FlagValue(flag.name, vscode.TreeItemCollapsibleState.Collapsed,
+      [
+        new FlagValue(`Open in Browser`, vscode.TreeItemCollapsibleState.None, [], "flagViewBrowser", flagUri),
+        new FlagValue(`key: ${flag.key}`, vscode.TreeItemCollapsibleState.None, [], "flagViewKey"),
+        new FlagValue(`on: ${flag.environments[this.config.env].on}`, vscode.TreeItemCollapsibleState.None, [], "flagViewToggle", "", flag.key, flag.name),
+        new FlagValue(`description: ${flag.description ? flag.description : ""}`, vscode.TreeItemCollapsibleState.None),
+        new FlagValue(`prerequisites: ${flag.environments[this.config.env].prerequisites}`, vscode.TreeItemCollapsibleState.None),
+      ]
+    )
+
+    var targets: Array<FlagValue> = []
+    if (flag.environments[this.config.env].targets) {
+    for(let i = 0; i<flag.environments[this.config.env].targets.length; i++) {
+        let curTarget = flag.environments[this.config.env].targets[i]
+        targets.push(
+          new FlagValue(`variation: ${flag.variations[curTarget.variation].name ? flag.variations[curTarget.variation].name : flag.variations[curTarget.variation].value}`, vscode.TreeItemCollapsibleState.None),
+          new FlagValue(`values: ${curTarget.values}`, vscode.TreeItemCollapsibleState.None),
         )
       }
-      item.children.push(new FlagValue(`variations:`, vscode.TreeItemCollapsibleState.Collapsed, variations))
+    }
+    item.children.push(new FlagValue(`targets:`, targets.length > 0 ? vscode.TreeItemCollapsibleState.Collapsed : vscode.TreeItemCollapsibleState.None, targets))
 
-      var rules: Array<FlagValue> = []
-      let parseRules = flag.environments[this.config.env].rules
+    var variations: Array<FlagValue> = []
+    for(let i = 0; i<flag.variations.length; i++) {
+      variations.push(
+        new FlagValue(`name: ${flag.variations[i].name ? flag.variations[i].name : flag.variations[i].value}`, vscode.TreeItemCollapsibleState.None),
+        new FlagValue(`value: ${flag.variations[i].value}`, vscode.TreeItemCollapsibleState.None),
+        new FlagValue(`description: ${flag.variations[i].description ? flag.variations[i].description : ""}`, vscode.TreeItemCollapsibleState.None)
+      )
+    }
+    item.children.push(new FlagValue(`variations:`, vscode.TreeItemCollapsibleState.Collapsed, variations))
+
+    var rules: Array<FlagValue> = []
+    let parseRules = flag.environments[this.config.env].rules
+    if (parseRules) {
       for(let i = 0;i<parseRules.length;i++){
         let curRule = parseRules[i]
         var clauses: Array<FlagValue> = []
@@ -103,22 +157,17 @@ export class ldFeatureFlagsProvider implements vscode.TreeDataProvider<FlagValue
           rules.push(new FlagValue(`variation: ${curRule.variation}`, vscode.TreeItemCollapsibleState.None))
         }
       }
-      item.children.push(new FlagValue(`rules:`, rules.length > 0 ? vscode.TreeItemCollapsibleState.Collapsed : vscode.TreeItemCollapsibleState.None , rules))
-
-      if (flag.defaults !== undefined) {
-        item.children.push(new FlagValue(`defaults:`, vscode.TreeItemCollapsibleState.Collapsed, [
-          new FlagValue(`onVariation: ${flag.defaults.onVariation}`, vscode.TreeItemCollapsibleState.None),
-          new FlagValue(`offVariation: ${flag.defaults.offVariation}`, vscode.TreeItemCollapsibleState.None),
-          ])
-        )
-      }
-      flagValues.push(item)
     }
-    this.flagValues = flagValues
-    this.refresh()
-  }
-  async start() {
-    this.getFlags()
+    item.children.push(new FlagValue(`rules:`, rules.length > 0 ? vscode.TreeItemCollapsibleState.Collapsed : vscode.TreeItemCollapsibleState.None , rules))
+
+    if (flag.defaults !== undefined) {
+      item.children.push(new FlagValue(`defaults:`, vscode.TreeItemCollapsibleState.Collapsed, [
+        new FlagValue(`onVariation: ${flag.defaults.onVariation}`, vscode.TreeItemCollapsibleState.None),
+        new FlagValue(`offVariation: ${flag.defaults.offVariation}`, vscode.TreeItemCollapsibleState.None),
+        ])
+      )
+    }
+    return item
   }
 }
 
@@ -127,34 +176,44 @@ export class FlagValue extends vscode.TreeItem {
     children: FlagValue[]|undefined;
     contextValue?: string
     uri?: string
+    flagKey?: string
+    flagParentName?: string
 
     constructor(
       public readonly label: string,
       public readonly collapsibleState: vscode.TreeItemCollapsibleState,
       children?: FlagValue[],
       contextValue?: string,
-      uri?: string
+      uri?: string,
+      flagKey?: string,
+      flagParentName?: string
     ) {
       super(label, collapsibleState);
       this.contextValue = contextValue
       this.children = children
       this.uri = uri
+      this.flagKey = flagKey
+      this.flagParentName = flagParentName
     }
 
     get tooltip(): string {
       return `${this.label}`
-      // let parseLabel = this.label.split(":")
-      // if (parseLabel.length > 0) {
-      //    return `${parseLabel[1].trim()}`;
-      // } else if (parseLabel.length === 0) {
-      //    return `${parseLabel[0].trim()}`;
-      // } else {
-      //   return `${this.label}`
-      // }
     }
 
-    // get description(): string {
-    //   return this.version;
-    // }
+}
 
+export function registerTreeviewRefreshCommand(
+	treeDataProvider: ldFeatureFlagsProvider
+  ): vscode.Disposable {
+	return vscode.commands.registerCommand(
+	  'launchdarkly.treeviewrefresh',
+	  (): void => {
+		treeDataProvider.refresh();
+		vscode.commands.executeCommand(
+		  'setContext',
+		  'launchdarkly:enableTreeview',
+		  getIsTreeviewEnabled()
+    );
+	  }
+	);
   }
