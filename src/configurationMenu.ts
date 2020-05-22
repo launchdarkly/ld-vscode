3167dfa56039714bdcaee55eb346c002d71a0170
Dan O'Brien <dobrien@launchdarkly.com>
2020-05-22 13:11:29 -0400

    back out remove token

--- src/configurationMenu.ts
+++ src/configurationMenu.ts
@@ -39,16 +39,14 @@ export class ConfigurationMenu {
 	shouldResume() {
 		// Required by multiStepInput
 		// Could show a notification with the option to resume.
-		return new Promise<boolean>(() => {});
+		return new Promise<boolean>(() => { });
 	}
 
 	async pickCurrentOrNewAccessToken(input: MultiStepInput) {
 		const existingTokenName = 'Use the existing access token';
-		const removeToken = 'Remove existing token';
 		const options = [
 			{ name: existingTokenName, key: 'xxxx' + this.currentAccessToken.substr(this.currentAccessToken.length - 6) },
 			{ name: 'Enter a new access token' },
-			{ name: removeToken },
 		].map(this.createQuickPickItem);
 
 		const pick = await input.showQuickPick({
@@ -66,11 +64,6 @@ export class ConfigurationMenu {
 			return (input: MultiStepInput) => this.pickProject(input);
 		}
 
-		if (pick.label === removeToken) {
-			this.config.setState('accessToken');
-			this.accessToken = '';
-			this.currentAccessToken = '';
-		}
 		return (input: MultiStepInput) => this.inputAccessToken(input);
 	}
 
