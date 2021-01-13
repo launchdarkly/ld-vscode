3e2b20ccb98fa2900c2ff7530eb302ed48e050dc
Rich Manalang <rmanalang@launchdarkly.com>
2021-01-13 12:03:50 -0800

    Change version format

--- build/prepare-beta.js
+++ build/prepare-beta.js
@@ -7,6 +7,6 @@ const package = require('../package.json');
 
 const dt = new Date;
 package.displayName = 'LaunchDarkly Beta'
-package.version = `${dt.getUTCFullYear()}.${dt.getUTCMonth()+1}.${dt.getUTCDate()}.${dt.getTime()}`;
+package.version = `${dt.getUTCFullYear()}.${dt.getUTCMonth()+1}.${dt.getTime()}`;
 fs.renameSync('package.json', 'package.orig.json');
 fs.writeFileSync('package.json', JSON.stringify(package));
