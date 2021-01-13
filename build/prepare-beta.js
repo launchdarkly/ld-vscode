1766299c6812a57c79339f592e4704c73bd02b6b
Rich Manalang <rmanalang@launchdarkly.com>
2021-01-13 12:06:32 -0800

    One more to conform to version specs

--- build/prepare-beta.js
+++ build/prepare-beta.js
@@ -7,6 +7,6 @@ const package = require('../package.json');
 
 const dt = new Date;
 package.displayName = 'LaunchDarkly Beta'
-package.version = `${dt.getUTCFullYear()}.${dt.getUTCMonth()+1}.${dt.getTime()}`;
+package.version = `${dt.getUTCFullYear()}.${dt.getUTCMonth()+1}.${Math.floor(dt.getTime()/1000)}`;
 fs.renameSync('package.json', 'package.orig.json');
 fs.writeFileSync('package.json', JSON.stringify(package));
