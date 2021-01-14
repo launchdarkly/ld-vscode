c2c5f4d3d6f38e947abc78813af1d750ead6d013
Rich Manalang <rmanalang@launchdarkly.com>
2021-01-14 09:43:17 -0800

    Publish beta under launchdarkly-beta id

--- build/prepare-beta.js
+++ build/prepare-beta.js
@@ -6,6 +6,7 @@ const fs = require('fs');
 const package = require('../package.json');
 
 const dt = new Date;
+package.name = 'launchdarkly-beta'
 package.displayName = 'LaunchDarkly Beta'
 package.version = `${dt.getUTCFullYear()}.${dt.getUTCMonth()+1}.${Math.floor(dt.getTime()/1000)}`;
 fs.renameSync('package.json', 'package.orig.json');
