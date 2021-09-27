4f03f481f24bd072d4aa3ee965b0457e4c1731ea
Dan O'Brien <dobrien@launchdarkly.com>
2021-09-27 09:15:23 -0400

    console log negative lens

--- src/providers/flagLens.ts
+++ src/providers/flagLens.ts
@@ -108,6 +108,10 @@ export class FlagCodeLensProvider implements vscode.CodeLensProvider {
 			while ((matches = regex.exec(text)) !== null) {
 				const line = document.lineAt(document.positionAt(matches.index).line);
 				const indexOf = line.text.indexOf(matches[0]);
+				if (line.lineNumber < 0 || indexOf < 0) {
+					console.log(`line number: ${line.lineNumber}`);
+					console.log(`index: ${indexOf}`);
+				}
 				const position = new vscode.Position(line.lineNumber, indexOf);
 				const range = document.getWordRangeAtPosition(position, new RegExp(this.regex));
 				const prospect = document.getText(range);
