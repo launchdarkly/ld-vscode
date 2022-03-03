0387e3a5278438f0ca621107c4bc672da3e87ee8
Dan O'Brien <dobrien@launchdarkly.com>
2022-03-03 12:42:01 -0500

    remove debugger

--- src/providers.ts
+++ src/providers.ts
@@ -7,7 +7,6 @@ import {
 	TextDocument,
 	workspace,
 	ConfigurationChangeEvent,
-	OutputChannel,
 } from 'vscode';
 
 import { Configuration } from './configuration';
@@ -15,14 +14,11 @@ import { LaunchDarklyAPI } from './api';
 import { FlagStore } from './flagStore';
 import { FlagNode } from './providers/flagListView';
 import { ClientSideEnable, refreshDiagnostics, subscribeToDocumentChanges } from './providers/diagnostics';
-import PubNub from 'pubnub';
 import globalClearCmd from './commands/clearGlobalContext';
 import configureLaunchDarkly from './commands/configureLaunchDarkly';
 import { extensionReload, setupComponents } from './utils';
 
 export const FLAG_KEY_REGEX = /[A-Za-z0-9][.A-Za-z_\-0-9]*/;
-
-type DebuggerWindows = Record<string, OutputChannel>;
 
 export async function register(
 	ctx: ExtensionContext,
@@ -48,37 +44,6 @@ export async function register(
 	}
 	if (typeof flagStore !== 'undefined') {
 		setupComponents(api, config, ctx, flagStore);
-
-		const Lddebugger = window.createOutputChannel('LaunchDarkly All');
-		const LddIdentify = window.createOutputChannel('LaunchDarkly Identify Debug');
-		const LddFeature = window.createOutputChannel('LaunchDarkly Feature Flag Debug');
-		const LddIndex = window.createOutputChannel('LaunchDarkly Index Debug');
-		const LddSummary = window.createOutputChannel('LaunchDarkly Summary Debug');
-		const debuggers = {
-			all: Lddebugger,
-			identify: LddIdentify,
-			features: LddFeature,
-			index: LddIndex,
-			summary: LddSummary,
-		};
-		const debugCipher = ctx.workspaceState.get('debugCipher') as string;
-
-		const ldConfig = workspace.getConfiguration('launchdarkly');
-		const subKey = ldConfig.get('debuggerKey', '').trim();
-
-		if (debugCipher && subKey) {
-			const pubnub = new PubNub({
-				subscribeKey: subKey,
-				cipherKey: debugCipher,
-				ssl: true,
-				useRandomIVs: false,
-			});
-			ctx.subscriptions.push(
-				commands.registerCommand('launchdarkly.enableDebugger', () => {
-					enableDebugger(ctx, debuggers, pubnub);
-				}),
-			);
-		}
 
 		ctx.subscriptions.push(
 			commands.registerCommand('launchdarkly.OpenFlag', (node: FlagNode) =>
@@ -112,193 +77,6 @@ export async function register(
 	subscribeToDocumentChanges(ctx, clientSideDiagnostics, aliases, flagStore);
 }
 
-const enableDebugger = (ctx: ExtensionContext, debuggers: DebuggerWindows, pubnub: PubNub) => {
-	const LddIdentify = debuggers['identify'];
-	const LddFeature = debuggers['features'];
-	const LddIndex = debuggers['index'];
-	const Lddebugger = debuggers['all'];
-	const LddSummary = debuggers['summary'];
-
-	const debugChannel = ctx.workspaceState.get('debugChannel') as string;
-
-	if (debugChannel) {
-		pubnub.unsubscribeAll();
-		Object.entries(debuggers).forEach(([, value]) => {
-			value.clear();
-			value.appendLine('Starting Debugger');
-		});
-		pubnub.addListener({
-			message: function (m) {
-				if (Array.isArray(m.message)) {
-					m.message.forEach(async (msg) => {
-						switch (msg.kind) {
-							case 'identify': {
-								const parsedMsg = parseIdentify(msg);
-								LddIdentify.append(parsedMsg);
-								Lddebugger.append('- - - -\nIdentify Event:\n' + parsedMsg);
-								break;
-							}
-							case 'feature': {
-								const parsedMsg = parseFeature(msg);
-								LddFeature.append(parsedMsg);
-								Lddebugger.append('- - - -\nFeature Event:\n' + parsedMsg);
-								break;
-							}
-							case 'index': {
-								const parsedMsg = parseIndex(msg);
-								LddIndex.append(parsedMsg);
-								Lddebugger.append('- - - -\nIndex Event:\n' + parsedMsg);
-								break;
-							}
-							case 'summary': {
-								const parsedMsg = parseSummary(msg);
-								LddSummary.append(parsedMsg);
-								Lddebugger.append('- - - -\nSummary Event:\n' + parsedMsg);
-								break;
-							}
-							default:
-								Lddebugger.append(JSON.stringify(msg));
-						}
-					});
-				} else {
-					Lddebugger.appendLine(m.message);
-				}
-			},
-		});
-
-		pubnub.subscribe({
-			channels: [debugChannel],
-		});
-
-		setTimeout(() => {
-			pubnub.unsubscribeAll();
-			const CLOSING_DEBUGGER = 'Disconnecting Debugger';
-			LddFeature.appendLine(CLOSING_DEBUGGER);
-			Lddebugger.appendLine(CLOSING_DEBUGGER);
-			LddIdentify.appendLine(CLOSING_DEBUGGER);
-			LddIndex.appendLine(CLOSING_DEBUGGER);
-		}, 5 * 60 * 1000);
-	}
-};
-
-type UserEvent = {
-	key: string;
-	email?: string;
-	anonymous?: boolean;
-	custom?: Record<string, unknown>;
-	privateAttrs?: Record<string, unknown>;
-};
-
-type IdentifyEvent = {
-	kind: string;
-	key: string;
-	user: UserEvent;
-};
-
-type IndexEvent = {
-	key: string;
-	anonymous?: boolean;
-	user: UserEvent;
-};
-
-type FeatureEvent = {
-	kind: string;
-	key: string;
-	value: unknown;
-	variation: number;
-	default: unknown;
-	creationDate: number;
-	version: number;
-	reason?: unknown;
-	userKey: string;
-};
-
-type SummaryEvent = {
-	startDate: number;
-	endDate: number;
-	features: Record<string, FeatureSummary>;
-};
-
-type FeatureCounter = {
-	version: number;
-	variation: number;
-	value: unknown;
-	count: number;
-};
-type FeatureSummary = {
-	default: number;
-	counters: Array<FeatureCounter>;
-};
-
-const parseSummary = (event: SummaryEvent) => {
-	const startDate = new Date(event.startDate);
-	const endDate = new Date(event.endDate);
-	let line = `Start Time: ${startDate.toString()} / End Time: ${endDate.toString()}\n`;
-
-	Object.entries(event.features).forEach(([key, feature]) => {
-		line = line + `	flag key: ${key}\n`;
-		line = line + `		default value: ${feature.default}\n`;
-		feature.counters.forEach((counter) => {
-			line = line + `		count: ${counter.count}\n`;
-			line = line + ` 		value: ${counter.value}\n`;
-			line = line + ` 		variation: ${counter.variation}\n`;
-			line = line + `		version ${counter.version}\n`;
-		});
-	});
-	return line;
-};
-const parseIdentify = (event: IdentifyEvent) => {
-	let line = `Key: ${event.key}
-	email: ${event.user.email}`;
-
-	if (event.user.custom) {
-		line =
-			line +
-			`
-	custom:\n`;
-		const customEvent = event.user.custom;
-		Object.entries(customEvent).forEach(([key, value]) => (line = line + `		${key}: ${value}\n`));
-	}
-
-	if (event.user.privateAttrs) {
-		line =
-			line +
-			`
-		private attributes:\n`;
-		const privateAttrs = event.user.privateAttrs;
-		Object.entries(privateAttrs).forEach(([key, value]) => (line = line + `		${key}: ${value}\n`));
-	}
-	line = line + `\n`;
-	return line;
-};
-
-const parseIndex = (event: IndexEvent) => {
-	let line = `key: ${event.user.key}\n`;
-	if (event.user.anonymous) {
-		line = line + `	anonymous: ${event.user.anonymous}\n`;
-	}
-
-	if (event.user.custom) {
-		line = line + `	custom:\n`;
-		const customEvent = event.user.custom;
-		Object.entries(customEvent).forEach(([key, value]) => (line = line + `		${key}: ${value}\n`));
-	}
-	return line;
-};
-const parseFeature = (event: FeatureEvent) => {
-	const d = new Date(event.creationDate);
-
-	let line = `${d.toString()}
-	key: ${event.key}
-	value: ${event.value}
-	variation: ${event.variation}\n`;
-
-	if (event.reason) {
-		line = line + `\n	reason: ${event.reason}`;
-	}
-
-	return line;
-};
 
 export function isPrecedingCharStringDelimiter(document: TextDocument, pos: Position): any {
 	throw new Error('Function not implemented.');
