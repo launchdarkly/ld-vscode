de52333c728b84aacc9fbf8fa5b142681b7e3ea4
Daniel OBrien <dobrien@launchdarkly.com>
2022-09-01 13:52:53 -0400

    lint fixes

--- src/providers/hover.ts
+++ src/providers/hover.ts
@@ -36,39 +36,39 @@ export class LaunchDarklyHoverProvider implements HoverProvider {
 	public async provideHover(document: TextDocument, position: Position): Promise<Hover | undefined> {
 		commands.executeCommand('setContext', 'LDFlagToggle', '');
 		if (this.config.enableHover && this.flagStore) {
-				const candidate = document.getText(document.getWordRangeAtPosition(position, FLAG_KEY_REGEX));
-				if (typeof candidate === 'undefined') {
-					return;
-				}
-				let aliases;
-				let foundAlias = [];
-				if (typeof this.aliases !== 'undefined') {
-					aliases = this.aliases?.getMap();
-					const aliasKeys = Object.keys(aliases || {});
-					const aliasArr = [...aliasKeys].filter((element) => element !== '');
-					foundAlias = aliasArr.filter((element) => candidate.includes(element));
-				} else {
-					aliases = [];
-				}
-				try {
-					let data =
-						(await this.flagStore.getFeatureFlag(candidate)) ||
-						(await this.flagStore.getFeatureFlag(kebabCase(candidate)));
-					console.log(data)
-					if (!data && aliases && foundAlias) {
-						data = await this.flagStore.getFeatureFlag(aliases[foundAlias[0]]);
-					} // We only match on first alias
-					if (data?.config) {
-						commands.executeCommand('setContext', 'LDFlagToggle', data.flag.key);
-						this.ctx.workspaceState.update('LDFlagKey', data.flag.key);
-						const hover = generateHoverString(data.flag, data.config, this.config, this.ctx);
-						return new Hover(hover);
-					}
-				} catch (e) {
-					return;
-				}
+			const candidate = document.getText(document.getWordRangeAtPosition(position, FLAG_KEY_REGEX));
+			if (typeof candidate === 'undefined') {
+				return;
 			}
-			return;
+			let aliases;
+			let foundAlias = [];
+			if (typeof this.aliases !== 'undefined') {
+				aliases = this.aliases?.getMap();
+				const aliasKeys = Object.keys(aliases || {});
+				const aliasArr = [...aliasKeys].filter((element) => element !== '');
+				foundAlias = aliasArr.filter((element) => candidate.includes(element));
+			} else {
+				aliases = [];
+			}
+			try {
+				let data =
+					(await this.flagStore.getFeatureFlag(candidate)) ||
+					(await this.flagStore.getFeatureFlag(kebabCase(candidate)));
+				console.log(data);
+				if (!data && aliases && foundAlias) {
+					data = await this.flagStore.getFeatureFlag(aliases[foundAlias[0]]);
+				} // We only match on first alias
+				if (data?.config) {
+					commands.executeCommand('setContext', 'LDFlagToggle', data.flag.key);
+					this.ctx.workspaceState.update('LDFlagKey', data.flag.key);
+					const hover = generateHoverString(data.flag, data.config, this.config, this.ctx);
+					return new Hover(hover);
+				}
+			} catch (e) {
+				return;
+			}
+		}
+		return;
 	}
 }
 
