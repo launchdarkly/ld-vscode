99f65da7f10484997fae55fce42c2392c59e8219
Daniel OBrien <dobrien@launchdarkly.com>
2024-02-03 21:52:31 -0500

    fix: flag lens variations
    add: osmo icon
    
    simplify multiple functions
    
    add: released flags highlights

--- src/ldExtensionConfiguration.ts
+++ src/ldExtensionConfiguration.ts
@@ -1,10 +1,11 @@
-import { ExtensionContext, TreeView } from 'vscode';
+import { ExtensionContext, StatusBarItem, TreeView } from 'vscode';
 import { Configuration } from './configuration';
 import { LaunchDarklyAPI } from './api';
 import { FlagStore } from './flagStore';
 import { FlagAliases } from './providers/codeRefs';
 import { LaunchDarklyAuthenticationSession } from './providers/authProvider';
 import { FlagTreeInterface, LaunchDarklyTreeViewProvider } from './providers/flagsView';
+import { LaunchDarklyReleaseProvider } from './providers/releaseViewProvider';
 
 export class LDExtensionConfiguration {
 	private static instance: LDExtensionConfiguration;
@@ -15,80 +16,98 @@ export class LDExtensionConfiguration {
 	private flagTreeView: TreeView<FlagTreeInterface>;
 	private flagView: LaunchDarklyTreeViewProvider;
 	private aliases?: FlagAliases;
+	private releaseView?: LaunchDarklyReleaseProvider;
 	private session?: LaunchDarklyAuthenticationSession;
+	private statusBar?: StatusBarItem;
 
 	private constructor(ctx: ExtensionContext) {
 		this.ctx = ctx;
 	}
 
-	public static getInstance(ctx?: ExtensionContext): LDExtensionConfiguration {
+	static getInstance(ctx?: ExtensionContext): LDExtensionConfiguration {
 		if (!LDExtensionConfiguration.instance) {
 			LDExtensionConfiguration.instance = new LDExtensionConfiguration(ctx);
 		}
 		return LDExtensionConfiguration.instance;
 	}
 
-	public setConfig(config: Configuration): void {
-		this.config = config;
-	}
-
-	public getConfig(): Configuration | undefined {
-		return this.config;
-	}
-
-	public setCtx(ctx: ExtensionContext): void {
-		this.ctx = ctx;
-	}
-
-	public getCtx(): ExtensionContext {
-		return this.ctx;
-	}
-
-	public setApi(api: LaunchDarklyAPI): void {
-		this.api = api;
-	}
-
-	public getApi(): LaunchDarklyAPI | undefined {
-		return this.api;
-	}
-
-	public setFlagStore(flagStore: FlagStore): void {
-		this.flagStore = flagStore;
-	}
-
-	public getFlagStore(): FlagStore | undefined {
-		return this.flagStore;
-	}
-
-	public setFlagTreeProvider(flagTreeProvider: TreeView<FlagTreeInterface>): void {
-		this.flagTreeView = flagTreeProvider;
-	}
-
-	public getFlagTreeProvider(): TreeView<FlagTreeInterface> | undefined {
-		return this.flagTreeView;
-	}
-
-	public setFlagView(flagView: LaunchDarklyTreeViewProvider): void {
-		this.flagView = flagView;
-	}
-
-	public getFlagView(): LaunchDarklyTreeViewProvider | undefined {
-		return this.flagView;
-	}
-
-	public setAliases(aliases: FlagAliases): void {
-		this.aliases = aliases;
-	}
-
-	public getAliases(): FlagAliases | undefined {
+	getAliases(): FlagAliases | undefined {
 		return this.aliases;
 	}
 
-	public setSession(session: LaunchDarklyAuthenticationSession): void {
+	setAliases(aliases: FlagAliases): void {
+		this.aliases = aliases;
+	}
+
+	getApi(): LaunchDarklyAPI | undefined {
+		return this.api;
+	}
+
+	setApi(api: LaunchDarklyAPI): void {
+		this.api = api;
+	}
+
+	getConfig(): Configuration | undefined {
+		return this.config;
+	}
+
+	setConfig(config: Configuration): void {
+		this.config = config;
+	}
+
+	getCtx(): ExtensionContext {
+		return this.ctx;
+	}
+
+	setCtx(ctx: ExtensionContext): void {
+		this.ctx = ctx;
+	}
+
+	getFlagStore(): FlagStore | undefined {
+		return this.flagStore;
+	}
+
+	setFlagStore(flagStore: FlagStore): void {
+		this.flagStore = flagStore;
+	}
+
+	getFlagTreeProvider(): TreeView<FlagTreeInterface> | undefined {
+		return this.flagTreeView;
+	}
+
+	setFlagTreeProvider(flagTreeProvider: TreeView<FlagTreeInterface>): void {
+		this.flagTreeView = flagTreeProvider;
+	}
+
+	getFlagView(): LaunchDarklyTreeViewProvider | undefined {
+		return this.flagView;
+	}
+
+	setFlagView(flagView: LaunchDarklyTreeViewProvider): void {
+		this.flagView = flagView;
+	}
+
+	getReleaseView(): LaunchDarklyReleaseProvider | undefined {
+		return this.releaseView;
+	}
+
+	setReleaseView(releaseView: LaunchDarklyReleaseProvider): void {
+		this.releaseView = releaseView;
+	}
+
+	getSession(): LaunchDarklyAuthenticationSession | undefined {
+		return this.session;
+	}
+
+	setSession(session: LaunchDarklyAuthenticationSession): void {
 		this.session = session;
 	}
 
-	public getSession(): LaunchDarklyAuthenticationSession | undefined {
-		return this.session;
+	getStatusBar(): StatusBarItem | undefined {
+		return this.statusBar;
+	}
+
+	setStatusBar(statusBar: StatusBarItem): void {
+		this.statusBar = statusBar;
 	}
 }
