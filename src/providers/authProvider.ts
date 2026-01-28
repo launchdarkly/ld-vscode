607ec9234b4cd3647eb57d5226c534a449cd3a1c
Cody Spath <cspath@launchdarkly.com>
2026-01-28 10:09:13 -0500

    feat: add out-of-the-box EU option for easy configuration

--- src/providers/authProvider.ts
+++ src/providers/authProvider.ts
@@ -197,7 +197,7 @@ export class LaunchDarklyAuthenticationProvider implements AuthenticationProvide
 				const stateId = uuid();
 				this._pendingStates.push(stateId);
 				const scopeString = scopes.join(' ');
-				const instances: QuickPickItem[] = [{ label: 'Commercial' }, { label: 'Federal' }, { label: 'Other' }];
+				const instances: QuickPickItem[] = [{ label: 'Commercial' }, { label: 'Federal' }, { label: 'EU' }, { label: 'Other' }];
 				const pickInstance = await window.showQuickPick(instances, {
 					title: 'LaunchDarkly Instance',
 					placeHolder: 'Select LaunchDarkly Instance',
@@ -217,6 +217,10 @@ export class LaunchDarklyAuthenticationProvider implements AuthenticationProvide
 					case 'Federal':
 						ldBaseUri = 'app.launchdarkly.us';
 						appBaseUri = `app.launchdarkly.us`;
+						break;
+					case 'EU':
+						ldBaseUri = 'app.eu.launchdarkly.com';
+						appBaseUri = `app.eu.launchdarkly.com`;
 						break;
 					case 'Other': {
 						let baseUris;
