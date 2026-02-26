149f3eff5bc7aa0dbe7b96fd1bae1381829fe3c7
Cody Spath <cspath@launchdarkly.com>
2026-02-26 09:56:16 -0500

    feat: add more analytics

--- src/commands/createFlag.ts
+++ src/commands/createFlag.ts
@@ -3,9 +3,11 @@ import { CreateFlagMenu } from '../createFlagMenu';
 import { CMD_LD_CREATE_FLAG } from '../utils/commands';
 import { registerCommand } from '../utils/registerCommand';
 import { ILDExtensionConfiguration } from '../models';
+import { analytics } from '../analytics';
 
 export default function createFlagCmd(config: ILDExtensionConfiguration): Disposable {
 	const createFlagCmd = registerCommand(CMD_LD_CREATE_FLAG, async () => {
+		analytics.track('flag-created');
 		const configurationMenu = new CreateFlagMenu(config);
 		await configurationMenu.collectInputs();
 	});
